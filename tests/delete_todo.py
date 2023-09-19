8df3100ce25ed8f2d2e12f60e203b6a605c5dcc5
Bia <Bia0406>
2023-09-19 08:59:49 +0300

    modify delete todo tests

--- tests/delete_todo.py
+++ tests/delete_todo.py
@@ -16,24 +16,33 @@ class TestDeleteTodo(unittest.TestCase):
         """
         Check:
         - the status code is 200
-        - we receive a todo with the specified todo_id
+        - the status code name is OK
+        - in response we have the key "id" equal with 150
+        - in response we have for the key "isDeleted" value "true"
         """
 
         response = self.requests_handler.delete_todo(todo_id=150)
-        expected_status_code = 404
-        expected_todos_response = 1
+        expected_status_code = 200
+        expected_response_status = "OK"
+        expected_response_key_id = 150
+        expected_response_key_is_deleted = True
         self.assertEqual(expected_status_code, response.status_code)
-        self.assertEqual(expected_todos_response, len(response.text[1]))
+        self.assertEqual(expected_response_status, response.reason)
+        self.assertEqual(expected_response_key_id, response.json()["id"])
+        self.assertEqual(expected_response_key_is_deleted, response.json()["isDeleted"])
 
     def test_delete_todo_when_id_exists_not_in_db(self):
         """
         Check:
         - the status code is 404
-        - in response I got the expected error
+        - the status code name is "Not Found"
+        - in response I got the expected message
         """
 
-        response = self.requests_handler.delete_todo(todo_id=151)
+        response = self.requests_handler.delete_todo(todo_id=152)
         expected_status_code = 404
-        expected_error = '!'
+        expected_response_status = "Not Found"
+        expected_error = "Todo with id '152' not found"
         self.assertEqual(expected_status_code, response.status_code)
-        self.assertEqual(expected_error, response.text[1])
+        self.assertEqual(expected_response_status, response.reason)
+        self.assertEqual(expected_error, response.json()["message"])
